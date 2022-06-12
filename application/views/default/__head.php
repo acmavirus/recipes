f9b2245365dc316388f811a5a094a33bfb9d93e7
AcmaTvirus <thuc.acma.98@gmail.com>
2022-06-12 08:56:20 +0700

    gg

--- application/views/default/__head.php
+++ application/views/default/__head.php
@@ -13,17 +13,25 @@
 
 <link rel="icon" href="<?php echo base_url(MEDIA_NAME) ?>favicon.ico">
 
-<!--Meta Facebook Page Other-->
+<!-- Facebook OG -->
 <meta property="og:type" content="article" />
 <meta property="og:title" content="<?php if (!empty($SEO->meta_title)) echo $SEO->meta_title; ?>" />
 <meta property="og:description" content="<?php if (!empty($SEO->meta_description)) echo $SEO->meta_description; ?>" />
 <meta property="og:image" content="<?php echo base_url('public/images/social.jpg'); ?>" />
 <meta property="og:url" content="<?php if (!empty($SEO->url)) echo $SEO->url; ?>" />
 
-<!--Facebook OG-->
+<!-- Google OG-->
 <link rel="canonical" href="<?php if (!empty($SEO->url)) echo $SEO->url; ?>" />
 <meta name="robots" content="<?php echo (!empty($SEO) && $SEO->is_robot == true) ? 'index,follow' : 'noindex,nofollow'; ?>" />
 <meta name="Googlebot-News" content="<?php echo (!empty($SEO) && $SEO->is_robot == true) ? 'index,follow' : 'noindex,nofollow'; ?>" />
+<!-- Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-152623543-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-152623543-1');
+</script>
 
 <!-- Link style -->
 <link rel="stylesheet" href="<?php echo base_url(VENDOR_PATH) ?>libs/bootstrap-5.0.2-dist/css/bootstrap.min.css">
