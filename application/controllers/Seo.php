96928614b4278080532a3bc21dba547130c92d7d
AcmaTvirus <thuc.acma.98@gmail.com>
2022-07-24 08:27:29 +0700

    fix seo post

--- application/controllers/Seo.php
+++ application/controllers/Seo.php
@@ -85,11 +85,11 @@ class Seo extends Public_Controller
     }
 
 
-    public function post($page = 1)
+    public function post()
     {
         $all = $this->_post_recipes->getData([
-            'page' => $page,
-            'limit' => $this->_limit_url
+            'page' => 1,
+            'limit' => 20000
         ]);
         $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml"  xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"/>');
         if (!empty($all)) {
